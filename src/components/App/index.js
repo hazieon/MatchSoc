065aee2e0bad4e2470d589cfc29ca57b50cf7f76
MellKay <59118345+MellKay@users.noreply.github.com>
2020-11-09 16:17:28 +0000

    Initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Happy Project Week</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Docs R Here
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
