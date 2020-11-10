adaa4b71724c7fdcec320f004c2e5bb74b05624f
moms-spaghetti <38544356+moms-spaghetti@users.noreply.github.com>
2020-11-10 20:52:21 +0000

    Update index.js

--- src/components/searchinput/index.js
+++ src/components/searchinput/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./SearchInput.css";
+import "./searchinput.css";
 function SearchInput({ setInput }) {
   return (
     <input placeholder="Search..." onKeyUp={(e) => setInput(e.target.value)} />
